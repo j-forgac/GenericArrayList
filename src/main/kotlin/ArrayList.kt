97ff978f8d4d1fd2f8b5b56b49fd3b74fc48c557
Ján Forgáč ml <jforgac04@gmail.com>
2022-02-24 22:41:08 +0100

    initial commit

---
+++ src/main/kotlin/ArrayList.kt
@@ -0,0 +1,49 @@
+class ArrayList<T>() {
+    private var array: Array<Any?> = arrayOfNulls(10)
+
+    var size = 0
+    val isEmpty : Boolean
+        get() = size == 0
+
+    fun add(element: T) {
+        if (size == array.size) expandArr()
+        array[size] = element
+        size++
+    }
+
+    fun set(index: Int, element: T) {
+        if (index < 0 || index >= size) throw IndexOutOfBoundsException("Index $index out of bounds for length $size")
+        else {
+            if (size == array.size) expandArr()
+            array[index] = element
+        }
+    }
+
+    fun contains(element: T): Boolean =
+        element in array
+
+    fun get(index: Int): T {
+        return if(index < 0 || index>=size) throw IndexOutOfBoundsException("Index $index out of bounds for length $size")
+        else array[index]!! as T
+    }
+
+    fun indexOf(element: T): Int =
+        array.indexOf(element)
+
+    fun lastIndexOf(element: T): Int =
+        array.lastIndexOf(element)
+
+    fun toArray(): Array<Any?>{
+        val arr = arrayOfNulls<Any>(size)
+        for(i in 0 until size){
+            arr[i] = array[i]!!
+        }
+        return arr
+    }
+
+    private fun expandArr(){
+        val buffer = array.copyOf()
+        array = Array(size * 3 / 2) { null }
+        buffer.forEachIndexed { newIndex, i ->  array[newIndex] = i}
+    }
+}
